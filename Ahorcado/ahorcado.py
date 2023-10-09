7a582190f96161455fb0753021e82a5d93b84a1e
aram-gonzalez-tec <aram.gonzalez@tec.mx>
2023-10-09 13:31:56 -0600

    Ahorcado

---
+++ Ahorcado/ahorcado.py
@@ -0,0 +1,26 @@
+def obtener_palabra(nombre_archivo : str) -> str:
+    pass
+
+def desplegar_tablero(palabra : str, correctas : list, incorrectas : list) -> None:
+    pass
+
+def tirada(palabra : str, letra : str, correctas : list, incorrectas : list) -> bool:
+    # True -> Tirada válida
+    # False -> Error
+    pass
+
+def juego_terminado(palabra : str, correctas : list, incorrectas : list) -> bool:
+    # True -> Terminó el juego
+    # False -> Sigue el juego
+    pass
+
+def registrar_marcador(nombre : str, puntaje : int) -> None:
+    pass
+
+def menu():
+    """Ahorcado
+    1) Jugar
+    2) Mostrar leaderboard
+    3) Salir
+    """
+    
